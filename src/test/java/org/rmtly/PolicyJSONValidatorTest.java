bb87b7d74ac771b02567d6f276a0a63759c733d6
Michał Kobiera <kobiera@zoho.com>
2024-04-22 16:51:22 +0200

    Add dependencies

--- src/test/java/org/rmtly/PolicyJSONValidatorTest.java
+++ src/test/java/org/rmtly/PolicyJSONValidatorTest.java
@@ -1,4 +1,32 @@
+package org.rmtly;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PolicyJSONValidatorTest {
-  
+
+    @Test
+    void validateIfAsteriskTest() {
+        String path = "asterisk.json";
+        assertFalse(PolicyJSONValidator.validate(path));
+    }
+
+    @Test
+    void validateIfNotAsteriskTest() {
+        String path = "";
+        assertTrue(PolicyJSONValidator.validate(path));
+    }
+
+    @Test
+    void damagedJSONFileTest() {
+        String path = "";
+        assertThrows(RuntimeException.class, () -> PolicyJSONValidator.validate(path));
+    }
+
+    @Test
+    void invalidJSONSchemaTest() {
+        String path = "";
+        assertThrows(RuntimeException.class, () -> PolicyJSONValidator.validate(path));
+    }
 }
