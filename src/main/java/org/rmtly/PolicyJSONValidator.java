37a5e7011ebe977e2ebb15cf6fe1ac42984f1303
Michał Kobiera <kobiera@zoho.com>
2024-04-22 16:39:26 +0200

    Add schame validation

--- src/main/java/org/rmtly/PolicyJSONValidator.java
+++ src/main/java/org/rmtly/PolicyJSONValidator.java
@@ -2,11 +2,15 @@ package org.rmtly;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -16,7 +20,7 @@ public class PolicyJSONValidator {
         try {
             jsonContent = new String(Files.readAllBytes(Paths.get(pathToInputFile)));
         } catch (IOException e) {
-            System.err.println("An error occurred while opening the input file!");
+            System.err.println("An error occurred while opening the input file");
             throw new RuntimeException(e);
         }
 
@@ -28,19 +32,26 @@ public class PolicyJSONValidator {
     }
 
     private static void checkJSONFile(String jsonContent) {
+        String schemaPath = "src/main/resources/schema.json";
         try {
             JSONObject jsonObject = new JSONObject(jsonContent);
+
+            // check json schema
+            InputStream schemaInputStream = new FileInputStream(schemaPath);
+            JSONObject rawSchema = new JSONObject(new JSONTokener(schemaInputStream));
+            Schema schema = SchemaLoader.load(rawSchema);
+
+            schema.validate(jsonObject);
+
         } catch (JSONException e) {
             System.err.println("JSON file is either not found, damaged, or contains invalid syntax");
             throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            System.err.println("Schema config file (src/main/resources/schema.json) not found");
+            throw new RuntimeException(e);
+        } catch (org.everit.json.schema.ValidationException e) {
+            System.err.println("Incorrect JSON schema");
+            throw new RuntimeException(e);
         }
-    }
-
-
-
-    public static void main(String[] args) {
-        String jsonFilePath = "src/test/resources/test.json";
-        boolean isValid = validate(jsonFilePath);
-        System.out.println(isValid);
     }
 }
